f8bc3a37fd5c8284fa9f7f00abf4a61f7138873e
Ilya S <public.ilya.s@gmail.com>
2014-04-13 16:26:01 +0700

    add some dummy code to the Dummy clas

--- tests/Dummy.php
+++ tests/Dummy.php
@@ -2,7 +2,60 @@
 
 class Dummy {
 
+    /**
+     * The Foo
+     *
+     * @var string
+     */
+    protected $foo;
 
+    /**
+     * The Bar
+     *
+     * @var integer
+     */
+    private static $bar;
+
+    /**
+     * The Baz
+     *
+     * @var array
+     */
+    public $baz;
+
+    /**
+     * Do Wow
+     *
+     * @param \stdClass $foo
+     * @param float $bar
+     * @param string $baz
+     * @return array
+     */
+    public function wow($foo, $bar, $baz)
+    {
+
+    }
+
+    /**
+     * See If Amaze
+     *
+     * @param mixed $amaze
+     * @return mixed|null
+     */
+    protected function so($amaze)
+    {
+
+    }
+
+    /**
+     * Do Amaze
+     *
+     * @return void
+     */
+    private static function amaze()
+    {
+
+    }
 
 }
 
