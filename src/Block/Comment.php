18003190a176d7915e80495a379ddaa0d656628d
Ilya S <public.ilya.s@gmail.com>
2014-04-13 16:40:16 +0700

    basic Comment

--- src/Block/Comment.php
+++ src/Block/Comment.php
@@ -2,7 +2,41 @@
 
 class Comment {
 
+    /**
+     * The comment itself
+     *
+     * @var string
+     */
+    protected $comment;
 
+    /**
+     * The constructor
+     *
+     * @param string $comment
+     * @return void
+     */
+    public function __construct($comment)
+    {
+        $this->setComment($comment);
+    }
+
+    /**
+     * Set the comment
+     *
+     * @param string $comment
+     * @return void
+     */
+    public function setComment($comment)
+    {
+        if ( ! \is_string($comment))
+        {
+            $message = 'Expected to receive a string, but got'.\gettype($comment);
+
+            throw new \InvalidArgumentException($message);
+        }
+
+        $this->comment = $comment;
+    }
 
 }
 
--- tests/Spec/Block/CommentSpec.php
+++ tests/Spec/Block/CommentSpec.php
@@ -5,9 +5,23 @@ use Prophecy\Argument;
 
 class CommentSpec extends ObjectBehavior {
 
+    function let()
+    {
+        $comment = 'dummy comment';
+
+        $this->beConstructedWith($comment);
+    }
+
     function it_is_initializable()
     {
         $this->shouldHaveType('Block\Comment');
+    }
+
+    function it_validates_the_passed_argument_and_sets_the_comment()
+    {
+        $this->shouldThrow('InvalidArgumentException')->duringSetComment(null);
+
+        $this->shouldNotThrow('InvalidArgumentException')->duringSetComment('dumb');
     }
 
 }
